d91ea0138faaf3f25ff9eae2395199cf9b635596
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2022-08-01 21:30:57 -0700

    stripe

--- flask_app/controllers/merchs.py
+++ flask_app/controllers/merchs.py
@@ -8,9 +8,8 @@ from flask_app import app
 import uuid as uuid
 from flask_app.models.user import User
 from flask_app.models.elements import Element
-import stripe
 
-
+import  stripe
 
 
 
@@ -19,6 +18,14 @@ import stripe
 
 def display_merch():
     navbar = Element
+
+    return render_template(
+        'merch.html',navbar=navbar,
+    )
+
+
+@app.route('/stripe_pay')
+def stripe_pay():
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
         line_items=[{
@@ -27,11 +34,50 @@ def display_merch():
         }],
         mode='payment',
         success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url=url_for('index', _external=True),
+        cancel_url=url_for('display_merch', _external=True),
     )
-    return render_template(
-        'merch.html',navbar=navbar, 
-        #checkout_session_id=session['id'], 
-        #checkout_public_key=app.config['STRIPE_PUBLIC_KEY']
-    )
+    return {
+        'checkout_session_id': session['id'],
+        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
+    }
 
+
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+
+@app.route('/stripe_webhook', methods=['POST'])
+def stripe_webhook():
+    print('WEBHOOK CALLED')
+
+    if request.content_length > 1024 * 1024:
+        print('REQUEST TOO BIG')
+        abort(400)
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = 'YOUR_ENDPOINT_SECRET'
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('INVALID PAYLOAD')
+        return {}, 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('INVALID SIGNATURE')
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
+        print(line_items['data'][0]['description'])
+
+    return {}
